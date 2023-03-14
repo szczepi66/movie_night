6e70d3895726812088734178c57275615bb2099f
Szczepan Markowski <szczepan@MacBook-Air-Szczepan.local>
2023-03-14 09:53:41 +0100

    uzupełnione ui bez funkcji

--- ui.py
+++ ui.py
@@ -1,26 +1,29 @@
-# import os
-# os.system('cls||clear')
-# ui = '''
-# --------------------------------
-# Nacisnij przyciski (1-3), aby wybrac co ciebie interesuje
+import os
+os.system('cls||clear')
+ui = '''
+--------------------------------
+Nacisnij przyciski (1-3), aby wybrac co ciebie interesuje
 
-# 1. Wybor filmu
-# 2. Nieznana opcja
-# 3. Wyjscie
+1. Wylosuj film
+2. Wybierz losowy film na podstawie kryteriów(gatunek, rok, ranking)
+3. Wyjscie
+4. Znajdź film
+8. Stwórz bazę danych ze strony filmweb.pl(top 500) 
+0. Wyjście
 
-# '''
-# exit = True
-# while exit == True:
-#     decision = int(input(ui))
-#     os.system('cls||clear')
-#     if decision == 1:
-#         print(1)
-#         exit = False
-#     elif decision == 2:
-#         print(2)
-#         exit = False
-#     elif decision == 3:
-#         print("Bye bye, see you soon")
-#         exit = False
-#     else:
-#         print("There is something wrong with your input try again\n")
+'''
+exit = True
+while exit == True:
+    decision = int(input(ui))
+    os.system('cls||clear')
+    if decision == 1:
+        print(1)
+        exit = False
+    elif decision == 2:
+        print(2)
+        exit = False
+    elif decision == 3:
+        print("Bye bye, see you soon")
+        exit = False
+    else:
+        print("There is something wrong with your input try again\n")
